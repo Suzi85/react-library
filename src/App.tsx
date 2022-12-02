6fedb9cba1d248cbd94e561f72ee7b5eb812d2ec
Suzan <s.sharifna@gmail.com>
2022-12-02 17:09:08 -0500

    added 2 more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,15 @@
 import './App.css';
 
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar'
 import React from 'react';
 
 function App() {
   return (
-    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
-      <div className='container-fluid'>
-        <span className='navbar-brand'>Luv 2 Read</span>
-        <button className='navbar-toggler' type='button'
-          data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
-          aria-controls='navbarNavDropdown' aria-expanded='false'
-          aria-label='Toggle Navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarNavDropdown'>
-          <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>Home</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>Search books</a>
-            </li>
-
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>Shelf</a>
-            </li>
-
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>Admin</a>
-            </li>
-
-          </ul>
-          <ul className='navbar-nav ms-auto'>
-
-            <li className='nav-item m-1'>
-              <button type='button' className='btn btn-outline-light'>Sign in</button>
-            </li>
-            :
-            <li>
-              <button className='btn btn-outline-light' >Logout</button>
-            </li>
-
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+    </div>
   );
 
 
---
+++ src/layouts/HomePage/ExploreTopBooks.tsx
@@ -0,0 +1,13 @@
+export const ExploreTopBooks = () => {
+      return (
+            <div className="p-5 mb-4 bg-dark header">
+                  <div className="container-fluid p-5 text-white d-flex justify-content-center align-item-center">
+                        <div>
+                              <h1 className="display-5 fw-bold">Find your next Advanture</h1>
+                              <p className="col-md-8 fs-4">Where to go?</p>
+                              <a type="button" className="btn main-color btn-lg text-white">Explore top books</a>
+                        </div>
+                  </div>
+            </div>
+      );
+}
